38b1867b8645cfcd87b2909e96c9e640aaa89b97
veggiecode <alexisao@hotmail.com>
2022-12-17 12:35:53 -0600

    Primera historia para OverviewPokemon

---
+++ src/components/OverviewPokemon/OverviewPokemon.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import OverviewPokemon from'./OverviewPokemon'
+
+export default {
+    title: 'Titulo',
+    component: OverviewPokemon
+}
+
+export const Default = () => <OverviewPokemon name='Hola'/>
